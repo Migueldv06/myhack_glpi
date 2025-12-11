111fd1b7c247f280cae641114a2d680b18f41e57
Miguel Domiciano Vieira <97977665+Migueldv06@users.noreply.github.com>
2025-12-11 16:58:23 -0300

    Add settings page for MyHack API configuration

---
+++ front/settings.php
@@ -0,0 +1,31 @@
+<?php
+
+include ('../../../inc/includes.php');  
+
+Session::checkLoginUser();
+
+Session::checkRight("config", UPDATE);
+
+Html::header(__('MyHack Settings', 'myhack'), $_SERVER['PHP_SELF'], "config","PluginMyhackMenu");
+
+if (isset($_POST['salvar_config'])) {
+    Config::setConfigurationValues('plugin:myhack', ['api_url' => $_POST['api_url']]);
+}
+
+$config = Config::getConfigurationValues('plugin:myhack');
+$url_atual = isset($config['api_url']) ? $config['api_url'] : '';
+
+echo "<div class='center'>";
+echo "<h1>Configurar API</h1>";
+    echo "<form method='post' action='".$_SERVER['PHP_SELF']."'>";
+
+        echo "<input type='hidden' name='_glpi_csrf_token' value='".Session::getNewCSRFToken()."'>";
+
+        echo "<label>Informe a URL da API:</label><br>";
+        echo "<input type='text' name='api_url' value='$url_atual' style='width: 400px; padding: 5px;' placeholder='https://...'><br><br>";
+        
+        echo "<input type='submit' name='salvar_config' value='Salvar Configuração' class='btn btn-primary'>";
+    echo "</form>";
+echo "</div>";
+
+Html::footer();
